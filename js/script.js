0fd5dd75152cd4066e0620c0d65170b63c20f51f
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-05 17:15:55 +0100

    feat:esercizio fatto

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// Title
+
+const titleElmen = document.getElementById("main-title");
+console.log(titleElmen);
+
+// Prendo gli elementi
+
+//immagine
+ const imgLampadina = document.getElementById("lampadina");
+ console.log(imgLampadina);
+
+ //Button
+ const bottone = document.querySelector(".btn");
+ console.log(bottone);
+
+ //
+
+ bottone.addEventListener("click", function() {
+
+    imgLampadina.src = "./img/yellow_lamp.png";
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
